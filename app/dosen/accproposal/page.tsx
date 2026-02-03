b4cdd771df53a6c9313706585a6dac088bcf87a2
Gerald Christopher Andreas <geraldchristopherandreas@MacBook-Air-Gerald.local>
2026-02-03 21:08:23 +0700

    coba push 5

--- app/dosen/accproposal/page.tsx
+++ app/dosen/accproposal/page.tsx
@@ -1,7 +1,7 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import React, { useEffect, useState } from "react";
-export const dynamic = "force-dynamic";
 import { 
   Search, 
   Bell, 
